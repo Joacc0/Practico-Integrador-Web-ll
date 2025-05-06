4ebdebfb534699a93814f87bdfd0879a9ee2370f
Laureano Joaquin Lucero <joaquinlucero194@gmail.com>
2025-05-06 18:37:13 -0300

    intentando que funcione render

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const rutas = require("./rutas");
+const rutas = require("../backend/rutas");
 
 const app = express();
 const PORT = 3000;
